798feb12d909c756b2175de0a932451fb05e95d6
peachparfait <peachparfait10@gmail.com>
2021-02-26 21:17:25 +0900

    アイコンmolecules作成

--- src/pages/templates/organisms/aboutMe.js
+++ src/pages/templates/organisms/aboutMe.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Profile from "./molecules/profile";
+import Icon from "./molecules/icon";
 
 const AboutMe = () => {
     return (
         <div className="aboutme">
           <Profile />
+          <Icon />
         </div>
     )
   }
---
+++ src/pages/templates/organisms/molecules/icon.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const Icon = () => {
+    return (
+        <div className="icon">icon</div>
+    )
+  }
+
+export default Icon;
