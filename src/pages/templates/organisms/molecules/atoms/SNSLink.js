9803ecd6c08754c1e379f5ad9fde1dfdc44a0917
peachparfait <peachparfait10@gmail.com>
2021-03-26 19:23:32 +0900

    背景色&間をななめに

---
+++ src/pages/templates/organisms/molecules/atoms/SNSLink.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const SNSLink = (props) => {
+    return (
+        <div className={props.classname}>{props.link}</div>
+    )
+  }
+
+export default SNSLink;
--- src/pages/templates/organisms/molecules/thumbnailObjects.js
+++ src/pages/templates/organisms/molecules/thumbnailObjects.js
@@ -1,9 +1,14 @@
 import * as React from "react";
 import MyName from "./atoms/myName";
-
+import SNSLink from "./atoms/SNSLink";
 const Ad = () => {
     return (
         <div className="ad">
+          <div className="snsLinks">
+            <SNSLink classname="github" link="github" />
+            <SNSLink classname="scratch" link="scratch" />
+            <SNSLink classname="twitter" link="coming soon" />
+          </div>
           <MyName />
         </div>
     )
