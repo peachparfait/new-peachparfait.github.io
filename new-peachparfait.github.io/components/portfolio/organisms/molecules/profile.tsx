1e472f01aa3e338ba7bb7357b8f001f14be26a06
peachparfait <peachparfait10@gmail.com>
2026-06-25 16:03:16 +0900

    プロフィールを記載

--- new-peachparfait.github.io/components/portfolio/organisms/molecules/profile.tsx
+++ new-peachparfait.github.io/components/portfolio/organisms/molecules/profile.tsx
@@ -1,7 +1,23 @@
 export default function Profile() {
   return (
     <div className="profile">
-      {/* プロフィール情報をここに書き込みます */}
+      {
+        <div>
+          <p>電気通信大学 情報理工学域 Ⅰ類
+          経営・社会情報学プログラム 3年</p>
+          <p>東京都</p>
+          <p>peachparfait10@gmail.com</p>
+          <p>Python/JavaScript/TypeScript/HTML/CSS</p>
+          <p>Web開発/UI/UX/データサイエンス/映像制作</p>
+          <h1>parfait9(pf9)</h1>
+          <p>2017年頃からScratchで活動</p>
+          <p>現在は主に技術系作品での名義</p>
+          <h1>PANAP</h1>
+          <p>電気通信大学バーチャルライブ研究会(VLL)に所属</p>
+          <p>VLLの活動や、映像制作での名義</p>
+
+      </div>
+      }
     </div>
   );
 }
